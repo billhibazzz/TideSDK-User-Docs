1f631e6ed33d27837b6e6cd04e76dc80c3efe510
Shebin Rawther <shebin.rawther@gmail.com>
2012-08-19 14:52:23 +0100

    Documentation added for database methods

--- source/API/Ti.Database.DB.js
+++ source/API/Ti.Database.DB.js
@@ -1,5 +1,34 @@
 /**
 * An object representing a TideSDK Database.
+*After opening a database (Ti.Database), you can use the properties of this object to interact with it.
+*
+*Querying Databases
+*------------------
+*Note - Please make sure that all queries are SQLite compatible. 
+*Please refer to the code examples below:
+*
+*		//Open the database first
+*		var db = Ti.Database.openFile(Ti.Filesystem.getFile(
+*									  Ti.Filesystem.getApplicationDataDirectory(), 'customdatabase.db'));	
+*		
+*		//Create a table and insert values into it
+*		db.execute("CREATE TABLE IF NOT EXISTS Users(id INTEGER, firstName TEXT)");
+*		db.execute("INSERT INTO Users VALUES(1,'Joe Bloggs')");		 
+*		
+*		//Select from Table
+*		var rows = db.execute("SELECT * FROM Users");
+*		while (rows.isValidRow()) {
+*			//Alert the value of fields id and firstName from the Users database
+*			alert('The user id is '+rows.fieldByName('id')+', and user name is '+rows.fieldByName('firstName'));
+*			rows.next();	
+*		}
+*
+*		//Release memory once you are done with the resultset and the database
+*		rows.close();
+*		db.close();	
+*		
+*
+*
 * @class Ti.Database.DB
 * @member Ti.Database
 */
